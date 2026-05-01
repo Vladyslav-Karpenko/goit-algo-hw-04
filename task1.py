4129028711a904a72bdf8a49fd86f90612dd1851
Vladyslav Karpenko <vladyslav.karpenko@ukr.net>
2026-05-01 12:57:36 +0200

    task1 updated

--- task1.py
+++ task1.py
@@ -8,21 +8,35 @@ from pathlib import Path
 # Результатом роботи функції є кортеж із двох чисел: загальної суми зарплат і середньої заробітної плати.
 
 
-def total_salary(salary_path: Path) -> tuple[int, int]:
-    salaries = []
+# 1 завдання. Загалом правильно. Тільки не треба було переводити зарплату в int.
+# Якщо явно не сказано, що будуть цілі числа, то краще використати float.
+# Будьте обережні з перетворенням типів - це завжи місце для потенційної помилки.
+# Також було сказано, що середню зарплату треба було порахувати точно, тому ділення націло тут недоречно.
+
+
+def total_salary(salary_path: Path) -> tuple[float, float]:
+
     try:
         with salary_path.open('r', encoding='utf-8') as file:
+            salaries = []
             for line in file:
-                _, salary = line.strip().split(',')
-                salaries.append(int(salary))
+                if not line.strip():
+                    continue
+                parts = line.strip().split(',')
+                if len(parts) != 2:
+                    continue
+                try:
+                    salaries.append(float(parts[1]))
+                except ValueError:
+                    continue
         if not salaries:
-            return (0, 0)
+            return (0.0, 0.0)
         total = sum(salaries)
-        average = total // len(salaries)
+        average = total / len(salaries)
         return (total, average)
     except FileNotFoundError as e:
         print(f"{salary_path} not found ERROR {e}")
-        return (0, 0)
+        return (0.0, 0.0)
 
 
 salary_file = Path('files/salary_file.txt')
@@ -31,4 +45,4 @@ salary_file = Path('files/salary_file.txt')
 total, average = total_salary(salary_file)
 
 print(
-    f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+    f"Загальна сума заробітної плати: {total:.2f}, Середня заробітна плата: {average:.2f}")
